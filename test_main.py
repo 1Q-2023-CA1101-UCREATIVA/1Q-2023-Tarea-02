48247f50e12275a0199b8bb8724742849366545d
Kevin Hernández Rostrán <98577482+profe-kevs@users.noreply.github.com>
2023-02-08 22:59:44 +0000

    feat: Add assignment

--- test_main.py
+++ test_main.py
@@ -1,6 +1,49 @@
-def func(x):
-    return x + 1
+from main import calcularDispositivoBebe
+import io
+import sys
+
+def test_portabebe():
+    capturedOutput = io.StringIO()                  # Create StringIO object
+    sys.stdout = capturedOutput                     #  and redirect stdout.
+
+    calcularDispositivoBebe(0.5, 10, 74)
+
+    sys.stdout = sys.__stdout__                     # Reset redirect.
+    print ('Captured', capturedOutput.getvalue())   # Now works as before.
+
+    assert capturedOutput.getvalue()== "Portabebé\n"
 
 
-def test_answer():
-    assert func(3) == 5
+def test_silla():
+    capturedOutput = io.StringIO()                  # Create StringIO object
+    sys.stdout = capturedOutput                     #  and redirect stdout.
+
+    calcularDispositivoBebe(2, 10, 78)
+
+    sys.stdout = sys.__stdout__                     # Reset redirect.
+    print ('Captured', capturedOutput.getvalue())   # Now works as before.
+
+    assert capturedOutput.getvalue()== "Silla\n"
+    
+    
+def test_con_respaldar():
+    capturedOutput = io.StringIO()                  # Create StringIO object
+    sys.stdout = capturedOutput                     #  and redirect stdout.
+
+    calcularDispositivoBebe(5,17,111)
+
+    sys.stdout = sys.__stdout__                     # Reset redirect.
+    print ('Captured', capturedOutput.getvalue())   # Now works as before.
+
+    assert capturedOutput.getvalue()== "Asiento con respaldar\n"
+
+def test_sin_respaldar():
+    capturedOutput = io.StringIO()                  # Create StringIO object
+    sys.stdout = capturedOutput                     #  and redirect stdout.
+
+    calcularDispositivoBebe(8,25,117)
+
+    sys.stdout = sys.__stdout__                     # Reset redirect.
+    print ('Captured', capturedOutput.getvalue())   # Now works as before.
+
+    assert capturedOutput.getvalue()== "Asiento sin respaldar\n"
